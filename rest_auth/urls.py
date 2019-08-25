4162ecc944590f8aac835cbe103f604baa3d54e0
Eugene C. Lee <lordpeara@gmail.com>
2019-08-25 21:44:31 +0900

    Fix reverse urls

--- rest_auth/urls.py
+++ rest_auth/urls.py
@@ -9,6 +9,8 @@ from .views import (
 )
 
 
+app_name = 'rest_auth'
+
 urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
